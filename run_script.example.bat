5431c35da8d7bc62720a8a2dbd9ead63e49ad3d2
kevin <kevinou488@gmail.com>
2024-11-04 02:29:27 -0800

    Fix bug that there might be chance with empty space in file name

--- run_script.example.bat
+++ run_script.example.bat
@@ -10,8 +10,42 @@ call .\Scripts\activate.bat
 :: Specify Python IO encoding.
 set PYTHONIOENCODING=utf-16
 
+:: ------------------ Date and Time Modifier ------------------------
+
+:: THIS CODE WILL DISPLAY A 2-DIGIT TIMESTAMP FOR USE IN APPENDING FILENAMES
+
+:: CREATE VARIABLE %TIMESTAMP%
+
+for /f "tokens=1-8 delims=.:/-, " %%i in ('echo exit^|cmd /q /k"prompt $D $T"') do (
+   for /f "tokens=2-4 skip=1 delims=/-,()" %%a in ('echo.^|date') do (
+set dow=%%i
+set %%a=%%j
+set %%b=%%k
+set %%c=%%l
+set hh=%%m
+set min=%%n
+set sec=%%o
+set hsec=%%p
+)
+)
+
+:: ensure that hour is always 2 digits
+
+if %hh%==0 set hh=00
+if %hh%==1 set hh=01
+if %hh%==2 set hh=02
+if %hh%==3 set hh=03
+if %hh%==4 set hh=04
+if %hh%==5 set hh=05
+if %hh%==6 set hh=06
+if %hh%==7 set hh=07
+if %hh%==8 set hh=08
+if %hh%==9 set hh=09
+
+:: assign timeStamp:
+:: Add the date and time parameters as necessary - " yy-mm-dd-dow-min-sec-hsec "
 :: Create a timestamp variable for the log filename.
-set timestamp=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set timeStamp=%yy%%mm%%dd%_%hh%-%min%-%sec%
 
 :: Define the path for the log file with the timestamp.
 set log_file=path\to\your\script\logs\update_%timestamp%.log
